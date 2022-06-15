67d4fcd2fe5a01fc346a3529624d1bf5cab8c0d5
rartaat <rartaat@gmail.com>
2022-06-15 20:53:14 +0200

    #task1 - add heroes template with routing

--- angular/demo/src/app/app-routing.module.ts
+++ angular/demo/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {RouterModule, Routes} from "@angular/router";
 import {UserListComponent} from "./user-list/user-list.component";
 import {UserFormComponent} from "./user-form/user-form.component";
 import {NgModule} from "@angular/core";
+import { HeroesComponent } from "./heroes/heroes.component";
 
 const routes: Routes = [
   { path: 'users', component: UserListComponent },
-  { path: 'adduser', component: UserFormComponent }
+  { path: 'adduser', component: UserFormComponent },
+  { path: 'heroes', component: HeroesComponent}
 ];
 
 @NgModule({
